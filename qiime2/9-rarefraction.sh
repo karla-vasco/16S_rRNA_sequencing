230f1e7456d88e5e70e100c8b4903737d69b4b00
karla-vasco <80479988+karla-vasco@users.noreply.github.com>
2022-05-04 12:49:12 -0400

    Add files via upload

---
+++ qiime2/9-rarefraction.sh
@@ -0,0 +1,30 @@
+#!/bin/bash --login
+
+######### Assign path variables ########
+WORK_DIRECTORY=/mnt/scratch/vascokar/mila/results/qiime2
+METADATA_PATH=/mnt/scratch/vascokar/mila/data/sample_metadata.txt
+
+########## Modules to Load ##########
+
+module purge
+module load Conda/3
+
+########## Code to Run ###########
+
+export PATH=$PATH:$HOME/anaconda3/bin
+conda init bash
+conda activate qiime2-2021.2
+
+cd $WORK_DIRECTORY
+
+qiime diversity alpha-rarefaction \
+  --i-table table-dn-97.qza \
+  --i-phylogeny insertion-tree.qza \
+  --p-max-depth 46999 \
+  --m-metadata-file $METADATA_PATH \
+  --o-visualization alpha-rarefaction.qzv
+
+conda deactivate
+
+##### Final time stamp ######
+echo Job finished at `date '+%T %a %d %b %Y'`
